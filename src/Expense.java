f39eef037f6a5b0882625542fc53f68bfd6b16c1
SANKURI NAGA CHARAN <86929699+NagaCharan2007@users.noreply.github.com>
2026-06-01 17:55:16 +0530

    Create Expense.java

---
+++ src/Expense.java
@@ -0,0 +1,55 @@
+public class Expense {
+
+    private int id;
+    private String date;
+    private double amount;
+    private String category;
+    private String description;
+
+    public Expense(int id, String date, double amount,
+                   String category, String description) {
+
+        this.id = id;
+        this.date = date;
+        this.amount = amount;
+        this.category = category;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String toString() {
+        return id + " | " + date + " | ₹" + amount +
+                " | " + category + " | " + description;
+    }
+}
